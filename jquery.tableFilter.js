f06bd8b37ec650e2f0f6b2e1ba9977b53b3a5450
xunxun10 <519230104@qq.com>
2022-11-20 22:24:28 +0800

    Update jquery.tableFilter.js
    修复低版本jquery下运行异常；
    增加对含有th的tr table支持；
    增加多列筛选支持

--- jquery.tableFilter.js
+++ jquery.tableFilter.js
@@ -1,75 +1,88 @@
 (function($){
     "use strict";
     $.fn.tableFilter = function(options) {
-	
-		//default settings
-	    var settings = $.extend({
-			tableID: '#filter-table',
-			filterID: '#filter',
-			filterCell: '.filter-cell',
-			autoFocus: false,
-			caseSensitive: false,
-			noResults: 'no results found',
-			columns: null
-		}, options);
-		
-		//auto focus on filter element if autofocs set to true
-		if(settings.autoFocus) {
-			$(settings.filterID).focus();
-		}
-		
-		//get table rows
-		var rowCount = $(settings.filterCell).parent().length;
-		
-		//get tablecolumns by counting td's in forst row unless passed as option
-		if(settings.columns === null) {
-			settings.columns = $(settings.tableID + ' > tbody > tr:first >td').length;
-		}
-		
-		//use case-sensitive matching unless changed by settings (default)
-		var contains = ':contains';
-		
-		if(!settings.caseSensitive) {
-			//create custom "icontains" selector for case insensitive search
-			$.expr[':'].icontains = $.expr.createPseudo(function(text) {
-			    return function(e) {
-			        return $(e).text().toUpperCase().indexOf(text.toUpperCase()) >= 0;
-			    };
-			});
-			contains = ':icontains';
-		}
-		
-		//bind eventListener to filter element
-		return this.find(settings.filterID).on("input", function() {
-			//get value of input
-			var filterString = $(this).val();
-		
-			//for each cell compare versus filter input
-			$(settings.filterCell).each(function(i){ //pass i as iterator
-				if($(this).is(contains + '(' + filterString + ')')) {
-					//check hidden rows for backspace operation
-					if($(this).is(':hidden')) {
-						$(this).parent().removeClass('filter-hidden').show();
-					}
-				} else {
-					$(this).parent().addClass('filter-hidden').hide();
-				}
-				//check if .each() iterations complete
-				if(rowCount === (i + 1)) {
-					//find rows with 'hidden' class and compare to row count if equal then display 'no results found' message
-					var hidden = $(settings.tableID).find('.filter-hidden').length;
-					if (hidden === rowCount) {
-						if ($('#noResults').is(':visible')) {
-							return; //do not display multiple "no results" messages
-						}
-						var newRow = $('<tr id="noResults"><td colspan="' + settings.columns +'"><em>' + settings.noResults + '</em></td></tr>').hide(); //row can be styled with CSS
-						$(settings.tableID).append(newRow);
-						newRow.show();
-					} else if ($('#noResults').is(':visible')) { 
-						$('#noResults').remove();
-					} 
-				}
-			}); 
-		});
-	};
+    
+        //default settings
+        var settings = $.extend({
+            tableID: '#filter-table',
+            filterID: '#filter',
+            filterCell: '.filter-cell',
+            autoFocus: false,
+            caseSensitive: false,
+            noResults: 'no results found',
+            columns: null
+        }, options);
+        
+        //auto focus on filter element if autofocs set to true
+        if(settings.autoFocus) {
+            $(settings.filterID).focus();
+        }
+        
+        //get table rows
+        var rowCount = $(settings.filterCell).parent().length;
+        
+        //get tablecolumns by counting td's in forst row unless passed as option
+        if(settings.columns === null) {
+            settings.columns = $('tr:first td,th', $(settings.tableID)).length;
+        }
+        
+        //use case-sensitive matching unless changed by settings (default)
+        var contains = ':contains';
+        
+        if(!settings.caseSensitive) {
+            //create custom "icontains" selector for case insensitive search
+            $.expr[':'].icontains = $.expr.createPseudo(function(text) {
+                return function(e) {
+                    return $(e).text().toUpperCase().indexOf(text.toUpperCase()) >= 0;
+                };
+            });
+            contains = ':icontains';
+        }
+        
+        //bind eventListener to filter element
+        return this.find(settings.filterID).on("input", function() {
+            //get value of input
+            var filterString = $(this).val();
+
+            if(filterString == ""){
+                $(settings.tableID + " .filter-hidden").removeClass('filter-hidden').show();
+                if ($('#noResults') != undefined) { 
+                    $('#noResults').remove();
+                } 
+                return;
+            }
+        
+            var g_find_flag = false;
+            //for each cell compare versus filter input
+            $(settings.tableID + " tr").each(function(i){ //pass i as iterator
+                // process td data
+                var find_flag = false;
+                $(settings.filterCell, $(this)).each(function(i){
+                    if($(this).is(contains + '(' + filterString + ')')) {
+                        find_flag = true;
+                        g_find_flag = true;
+                    }
+                })
+                if(find_flag) {
+                    //check hidden rows for backspace operation
+                    if($(this).is(':hidden')) {
+                        $(this).removeClass('filter-hidden').show();
+                    }
+                } else {
+                    $(this).addClass('filter-hidden').hide();
+                }
+            }); 
+            //find rows with 'hidden' class and compare to row count if equal then display 'no results found' message
+            if (!g_find_flag) {
+                if ($('#noResults').is(':visible')) {
+                    return; //do not display multiple "no results" messages
+                }
+                var newRow = $('<tr id="noResults"><td colspan="' + settings.columns +'"><em>' + settings.noResults + '</em></td></tr>').hide(); //row can be styled with CSS
+                $(settings.tableID).append(newRow);
+                newRow.show();
+            } else if ($('#noResults') != undefined) { 
+                $('#noResults').remove();
+            } 
+        });
+    };
 }(jQuery));
